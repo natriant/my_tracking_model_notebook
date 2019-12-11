46334ac1eea93f863c2a439bf405777bbd20adbe
Natalia Triantafyllou <natalia.triantafyllou@cern.ch>
2019-12-11 11:43:19 +0100

    correct implementations for functions in the long plane

--- utils/.ipynb_checkpoints/my_machines-checkpoint.py
+++ utils/.ipynb_checkpoints/my_machines-checkpoint.py
@@ -2,7 +2,7 @@ from dotted_dict import DottedDict
 from my_elements import *
 
 
-def my_machine(Qx_init, Qy_init, segments, k3, turn, bunch, twiss, flag_oct, flag_noise, flag_BB, flag_feedback, max_aperture_value, Delta = 0., ksi = 0., gain = 0., sigmax = 0., sigmapx = 0., sigmay=0., sigmapy = 0.):
+def my_machine(Qx_init, Qy_init, segments, k3, turn, bunch, twiss, rf, long_motion_parameters, flag_oct, flag_noise, flag_BB, flag_feedback, max_aperture_value, Delta = 0., ksi = 0., gain = 0., sigmax = 0., sigmapx = 0., sigmay=0., sigmapy = 0.):
     if turn == 1:
         print('flag_oct {}, flag_noise {}, flag_BB {}, flag_feedback {}'.format(flag_oct, flag_noise, flag_BB, flag_feedback))
         print('aperture limit is {} [m]'.format(max_aperture_value))
@@ -29,9 +29,14 @@ def my_machine(Qx_init, Qy_init, segments, k3, turn, bunch, twiss, flag_oct, fla
             if turn ==1:
                 print('g',gain)
             bunch.x, bunch.px, bunch.y, bunch.py = feedback_system_map(gain, sigmapx, sigmapy, x= bunch.x, px = bunch.px, y = bunch.y, py=bunch.py)
+            
+    # Longitudinal motion, as long as there is no dispersion, it can be seperetely.
+    bunch.z, bunch.delta = drift(long_motion_parameters.eta, long_motion_parameters.C0, bunch.z, bunch.delta)
+    bunch.z, bunch.delta = RF_map(long_motion_parameters.P0_C, long_motion_parameters.m0, long_motion_parameters.beta_0, rf.f_RF, long_motion_parameters.c, rf.lag, rf.V_RF, long_motion_parameters.E_0, bunch.z, bunch.delta)
+            
     return bunch
 
-def my_machine_with_detuners(Qx_init, Qy_init, k3_equivalent_x, k3_equivalent_y, turn, bunch, twiss, flag_detuner, flag_noise, flag_BB, flag_feedback, max_aperture_value, Delta = 0., ksi = 0., gain = 0., sigmax = 0., sigmapx = 0., sigmay=0., sigmapy = 0.):
+def my_machine_with_detuners(Qx_init, Qy_init, k3_equivalent_x, k3_equivalent_y, turn, bunch, twiss, rf, long_motion_parameters, flag_detuner, flag_noise, flag_BB, flag_feedback, max_aperture_value, Delta = 0., ksi = 0., gain = 0., sigmax = 0., sigmapx = 0., sigmay=0., sigmapy = 0.):
     if turn == 1:
         print('flag_detuner {}, flag_noise {}, flag_BB {}, flag_feedback {}'.format(flag_detuner, flag_noise, flag_BB, flag_feedback))
         print('aperture limit is {} [m]'.format(max_aperture_value))
@@ -42,7 +47,7 @@ def my_machine_with_detuners(Qx_init, Qy_init, k3_equivalent_x, k3_equivalent_y,
     #else:    
     if flag_detuner:
         if turn == 1 :
-                print('k3 equivalent x {}, k3 equivalent y'.format(k3_equivalent_x, k3_equivalent_y))
+                print('k3 equivalent x {}, k3 equivalent y {}'.format(k3_equivalent_x, k3_equivalent_y))
         particles_tunes_x, particles_tunes_y = amplitude_detuning(k3_equivalent_x, k3_equivalent_y, twiss,x = bunch.x, px = bunch.px, y = bunch.y, py=bunch.py)
         bunch.x, bunch.px, bunch.y, bunch.py = rotation_with_detuners(particles_tunes_x, particles_tunes_y, Qx_rot = Qx_init, Qy_rot = Qy_init, twiss = twiss, x = bunch.x, px = bunch.px, y = bunch.y, py=bunch.py)
         
@@ -65,4 +70,10 @@ def my_machine_with_detuners(Qx_init, Qy_init, k3_equivalent_x, k3_equivalent_y,
             print('g',gain)
         bunch.x, bunch.px, bunch.y, bunch.py = feedback_system_map(gain, sigmapx, sigmapy, x= bunch.x, px = bunch.px, y = bunch.y, py=bunch.py)        
         
+        
+        
+    # Longitudinal motion, as long as there is no dispersion, it can be seperetely.
+    bunch.z, bunch.delta = drift(long_motion_parameters.eta, long_motion_parameters.C0, bunch.z, bunch.delta)
+    bunch.z, bunch.delta = RF_map(long_motion_parameters.P0_C, long_motion_parameters.m0, long_motion_parameters.beta_0, rf.f_RF, long_motion_parameters.c, rf.lag, rf.V_RF, long_motion_parameters.E_0, bunch.z, bunch.delta)
+
     return bunch
